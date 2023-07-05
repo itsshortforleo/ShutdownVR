9e8689113ce8f1105ab0ec053e3ba9bc809cfeb6
Leonardo Lopez <3859260+itsshortforleo@users.noreply.github.com>
2023-07-05 09:03:08 -0700

    Add files via upload

---
+++ ShutdownVR/CloseSteamVR.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ShutdownVR
+{
+    class CloseSteamVR
+    {
+        public static void CloseSteam()
+        {
+            try
+            {
+                var p = Process.GetProcessesByName("vrmonitor").First();
+                foreach (var handle in EnumerateProcessWindowHandles(Process.GetProcessesByName("vrmonitor").First().Id))
+                {
+                    StringBuilder message = new StringBuilder(1000);
+                    SendMessage(handle, WM_CLOSE, message.Capacity, message);
+                    p.WaitForExit();
+                    Debug.WriteLine(message);
+                    Shutdown();
+                }
+                //Console.WriteLine("Hello World!");
+            }
+            catch (System.Exception) { }
+        }
+        private const uint WM_CLOSE = 0x10;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam,
+            StringBuilder lParam);
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn,
+            IntPtr lParam);
+
+        static IEnumerable<IntPtr> EnumerateProcessWindowHandles(int processId)
+        {
+            try
+            {
+                var handles = new List<IntPtr>();
+
+                foreach (ProcessThread thread in Process.GetProcessById(processId).Threads)
+                    EnumThreadWindows(thread.Id,
+                        (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+
+                return handles;
+            }
+            catch (System.Exception) { return null; }
+        }
+
+        static void Shutdown()
+        {
+            Process.Start("shutdown.exe", "-s -t 00");
+        }
+    }
+}
---
+++ ShutdownVR/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShutdownVR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            CloseSteamVR.CloseSteam();
+            //Console.WriteLine("Hello World 2!");
+        }
+    }
+}
